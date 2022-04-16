b83a5cae549a401d825cf77c73615d7caa02c5f7
shen494157765 <shen494157765@gmail.com>
2022-04-16 00:33:59 -0400

    add 0.9 base,  (0.75 0.8 0.85) tiny

---
+++ scripts/run-deit-base-keeprate0.9-load-pretrain-finetune.sh
@@ -0,0 +1,31 @@
+cd ../evit/
+
+# Train as EViT, load pretrain model and finetune
+# Keep Rate 0.9
+
+data_path=${1:-"/mnt/dataset/imagenet"}
+save_path="../checkpoints/exp-deit-base-keeprate0.9-load-pretrain-finetune"
+mkdir -p $save_path
+mkdir logs
+
+CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
+nohup \
+python3 -m torch.distributed.launch --nproc_per_node=8 \
+        --use_env main.py \
+        --model deit_base_patch16_shrink_base \
+        --base_keep_rate 0.9 \
+        --input-size 224 \
+        --sched cosine \
+        --lr 2e-5 \
+        --min-lr 2e-6 \
+        --weight-decay 1e-6 \
+        --batch-size 256 \
+        --shrink_start_epoch 0 \
+        --warmup-epochs 0 \
+        --shrink_epochs 0 \
+        --epochs 30 \
+        --dist-eval \
+        --finetune https://dl.fbaipublicfiles.com/deit/deit_base_patch16_224-b5f2ef4d.pth \
+        --data-path ${data_path} \
+        --output_dir ${save_path} \
+2>&1 | tee ../logs/exp-deit-base-keeprate0.9-load-pretrain-finetune.txt
---
+++ scripts/run-deit-tiny-keeprate0.8-LTH.sh
@@ -0,0 +1,28 @@
+cd ../evit/
+
+# Train from scratch as LTH using the pretrained EViT model as "teacher"
+# Keep Rate 0.8
+
+data_path=${1:-"/mnt/dataset/imagenet"}
+save_path="../checkpoints/exp-deit-tiny-keeprate0.8-LTH"
+mkdir -p $save_path
+mkdir logs
+
+
+CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
+nohup \
+python3 -m torch.distributed.launch --nproc_per_node=8 \
+        --use_env main.py \
+        --model deit_tiny_patch16_shrink_base \
+        --lottery ../checkpoints/exp-deit-tiny-keeprate0.8-load-pretrain-finetune/best_checkpoint.pth \
+        --base_keep_rate 0.8 \
+        --input-size 224 \
+        --batch-size 128 \
+        --warmup-epochs 5 \
+        --shrink_start_epoch 10 \
+        --shrink_epochs 100 \
+        --epochs 300 \
+        --dist-eval \
+        --data-path ${data_path} \
+        --output_dir ${save_path} \
+2>&1 | tee ../logs/exp-deit-tiny-keeprate0.8-LTH.txt 2>&1 &
