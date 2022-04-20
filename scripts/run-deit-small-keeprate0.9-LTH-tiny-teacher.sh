002e9b8d182b22b917decd591798073de2d29e6b
shen494157765 <shen494157765@gmail.com>
2022-04-20 17:55:20 -0400

    add tiny teacher experiment

--- scripts/run-deit-small-keeprate0.9-LTH-tiny-teacher.sh
+++ scripts/run-deit-small-keeprate0.9-LTH-tiny-teacher.sh
@@ -1,20 +1,18 @@
 cd ../evit/
 
-# Train from scratch as LTH using the pretrained DeiT Tiny model as "teacher"
+# Train from scratch as LTH using the DeiT Tiny as "teacher"
 # Keep Rate 0.9
 
-data_path=${1:-"/mnt/dataset/imagenet"}
-save_path="../checkpoints/exp-deit-base-keeprate0.9-LTH-tiny-teacher"
-log_path="../logs"
+data_path="/home/ImageNet"
+save_path="../checkpoints/exp-deit-small-keeprate0.9-LTH-tiny-teacher"
 mkdir -p $save_path
-mkdir -p $log_path
 
 
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
 nohup \
 python3 -m torch.distributed.launch --nproc_per_node=8 \
         --use_env main.py \
-        --model deit_base_patch16_shrink_base \
+        --model deit_small_patch16_shrink_base \
         --lottery ../checkpoints/exp-deit-tiny-keeprate0.9-LTH/best_checkpoint.pth \
         --lottery-model-type deit_tiny_patch16_shrink_base \
         --base_keep_rate 0.9 \
@@ -27,4 +25,5 @@ python3 -m torch.distributed.launch --nproc_per_node=8 \
         --dist-eval \
         --data-path ${data_path} \
         --output_dir ${save_path} \
-2>&1 | tee ../logs/exp-deit-base-keeprate0.9-LTH-tiny-teacher.txt 2>&1 &
+        --seed 1 \
+> ../scripts/exp-deit-small-keeprate0.9-LTH-tiny-teacher.txt 2>&1 &
