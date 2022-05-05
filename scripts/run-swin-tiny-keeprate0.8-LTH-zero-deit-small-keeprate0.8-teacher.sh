f8ee7c88e80946b359a30304c04985d2892cce67
shen494157765 <shen494157765@gmail.com>
2022-05-04 21:01:33 -0400

    add 0.8 swin

---
+++ scripts/run-swin-tiny-keeprate0.8-LTH-zero-deit-small-keeprate0.8-teacher.sh
@@ -0,0 +1,21 @@
+cd ../swin/
+
+data_path=${1:-"/home/ImageNet"}
+save_path="../checkpoints/exp-swin-tiny-keeprate0.8-LTH-zero-deit-small-keeprate0.8-teacher"
+mkdir -p $save_path
+
+CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
+nohup \
+python3 -m torch.distributed.launch --nproc_per_node=8 \
+        main.py \
+        --cfg configs/swin_tiny_patch4_window7_224.yaml \
+        --lottery ../checkpoints/exp-deit-small-keeprate0.8-load-pretrain-finetune/best_checkpoint.pth \
+        --lottery-model-type deit_small_patch16_shrink_base \
+        --base_keep_rate 0.8 \
+        --sparse-eval-with-zero \
+        --batch-size 128 \
+        --epochs 300 \
+        --amp-opt-level O1 \
+        --output ${save_path} \
+        --data-path ${data_path} \
+> ../scripts/exp-swin-tiny-keeprate0.8-LTH-zero-deit-small-keeprate0.8-teacher.txt 2>&1 &
---
+++ scripts/run-swin-tiny-keeprate0.8-RR-zero.sh
@@ -0,0 +1,21 @@
+cd ../swin/
+
+data_path=${1:-"/home/ImageNet"}
+save_path="../checkpoints/exp-swin-tiny-keeprate0.8-RR-zero"
+mkdir -p $save_path
+
+CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
+nohup \
+python3 -m torch.distributed.launch --nproc_per_node=8 \
+        main.py \
+        --cfg configs/swin_tiny_patch4_window7_224.yaml \
+        --random \
+        --lottery-model-type deit_small_patch16_shrink_base \
+        --base_keep_rate 0.8 \
+        --sparse-eval-with-zero \
+        --batch-size 128 \
+        --epochs 300 \
+        --amp-opt-level O1 \
+        --output ${save_path} \
+        --data-path ${data_path} \
+> ../scripts/exp-swin-tiny-keeprate0.8-RR-zero.txt 2>&1 &
