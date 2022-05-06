3e2e4a42b6cf0032c8b09b12090a70e89e694f28
shen494157765 <shen494157765@gmail.com>
2022-05-06 16:09:28 -0400

    update parameters for gmacs

--- scripts/run-deit-small-EViT-compare-2.26GMACs.sh
+++ scripts/run-deit-small-EViT-compare-2.26GMACs.sh
@@ -3,7 +3,7 @@ cd ../evit/
 # Use EViT pretrain as ablation study
 
 data_path="/home/ImageNet"
-save_path="../checkpoints/exp-deit-small-EViT-compare-2.52GMACs"
+save_path="../checkpoints/exp-deit-small-EViT-compare-2.26GMACs"
 mkdir -p $save_path
 
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
@@ -11,15 +11,11 @@ nohup \
 python3 -m torch.distributed.launch --nproc_per_node=8 \
         --use_env main.py \
         --model deit_small_patch16_shrink_base \
-        --base_keep_rate 0.61 \
+        --base_keep_rate 0.54 \
         --input-size 224 \
         --batch-size 128 \
-        --adjust-keep-rate \
-        --warmup-epochs 5 \
-        --shrink_start_epoch 10 \
-        --shrink_epochs 100 \
         --epochs 300 \
         --dist-eval \
         --data-path ${data_path} \
         --output_dir ${save_path} \
-> ../scripts/exp-deit-small-EViT-compare-2.52GMACs.txt 2>&1 &
+> ../scripts/exp-deit-small-EViT-compare-2.26GMACs.txt 2>&1 &
