7d67785b3fb64fd867118a021f8190704b145b09
shen494157765 <shen494157765@gmail.com>
2022-08-02 14:30:19 -0700

    update imagenet training code

--- scripts-cnn/run-resnet50-pretrain.sh
+++ scripts-cnn/run-resnet50-pretrain.sh
@@ -2,29 +2,50 @@
 
 cd ../cnn/
 
-seed=1
+seed=914
 dataset="imagenet"
-num_classes=1000
-input_resolution=224
-epochs=120
-batch_size_per_gpu=96
+epochs=90
+batch_size_per_gpu=128
+lr=0.2048
+
 
 name=resnet50-${dataset}
 save_dir=../checkpoints/${name}-${epochs}epochs-bs${batch_size_per_gpu}/pretrain/
 mkdir -p ${save_dir}
 
-CUDA_VISIBLE_DEVICES="0" \
+CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
 nohup \
-python3 -m torch.distributed.launch --nproc_per_node=4 --use_env \
-  main_only_resnet50_pretrain.py \
-  --dataset ${dataset} --num_classes ${num_classes} \
-  --dist_mode distribute --workers_per_gpu 6 \
-  --input_image_size ${input_resolution} --epochs ${epochs} --warmup 5 \
-  --optimizer sgd --bn_momentum 0.01 --wd 4e-5 --nesterov --weight_init custom \
-  --label_smoothing --random_erase --mixup --auto_augment \
-  --lr_per_256 0.1 --target_lr_per_256 0.0 --lr_mode cosine \
-  --batch_size_per_gpu ${batch_size_per_gpu} \
-  --seed ${seed} \
-  --save_dir ${save_dir} \
+python3 -m torch.distributed.launch --nproc_per_node=8 --use_env \
+    main.py \
+    --model resnet50 \
+    --seed ${seed} \
+    --lr ${lr]} \
+    --epochs ${epochs} \
+    --batch-size ${batch_size_per_gpu} \
+    --output ${save_dir} \
+    --data_dir /home/ImageNet \
+    --dataset ImageNet \
+    --amp \
 > ../scripts-cnn/output-${name}-${epochs}epochs-bs${batch_size_per_gpu}-pretrain.log 2>&1 &
 
+
+
+
+
+
+
+
+
+
+#CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
+#python3 -m torch.distributed.launch --nproc_per_node=8 --use_env \
+#    main.py \
+#    --model resnet50 \
+#    --seed 914 \
+#    --lr 0.2048 \
+#    --epochs 90 \
+#    --batch-size 128 \
+#    --output checkpoints \
+#    --data_dir /home/ImageNet \
+#    --dataset ImageNet \
+#    --amp
