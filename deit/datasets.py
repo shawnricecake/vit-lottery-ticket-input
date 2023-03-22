a366c07a829c9d07de7e0fa1cf6c78c466c72fa5
shen494157765 <shen494157765@gmail.com>
2023-03-21 22:33:10 -0400

    add cifar dataset

--- deit/datasets.py
+++ deit/datasets.py
@@ -61,9 +61,12 @@ class INatDataset(ImageFolder):
 def build_dataset(is_train, args):
     transform = build_transform(is_train, args)
 
-    if args.data_set == 'CIFAR':
-        dataset = datasets.CIFAR100(args.data_path, train=is_train, transform=transform)
+    if args.data_set == 'CIFAR100':
+        dataset = datasets.CIFAR100(args.data_path, train=is_train, transform=transform, download=True)
         nb_classes = 100
+    elif args.data_set == 'CIFAR10':
+        dataset = datasets.CIFAR10(args.data_path, train=is_train, transform=transform, download=True)
+        nb_classes = 10
     elif args.data_set == 'IMNET':
         if args.use_lmdb:
             root = os.path.join(args.data_path, 'train.lmdb' if is_train else 'val.lmdb')
--- deit/main.py
+++ deit/main.py
@@ -163,7 +163,7 @@ def get_args_parser():
     # Dataset parameters
     parser.add_argument('--data-path', default='/datasets01/imagenet_full_size/061417/', type=str,
                         help='dataset path')
-    parser.add_argument('--data-set', default='IMNET', choices=['CIFAR', 'IMNET', 'INAT', 'INAT19'],
+    parser.add_argument('--data-set', default='IMNET', choices=['CIFAR10', 'CIFAR100', 'IMNET', 'INAT', 'INAT19'],
                         type=str, help='Image Net dataset path')
     parser.add_argument('--use-lmdb', action='store_true', help='use Image Net lmdb dataset (for data-set==IMNET)')
     parser.add_argument('--inat-category', default='name',
